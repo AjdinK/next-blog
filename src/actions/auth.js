05b968f78b8868b576dea0af74aefaea13656354
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2024-12-18 02:22:54 +0100

    Register Form Validation with Zod

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,10 +1,18 @@
 "use server";
+
+import { RegisterFormSchema } from "@/lib/rules";
+
 export async function register(state, formData) {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const validatedFields = RegisterFormSchema.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+    confirmPassword: formData.get("confirmPassword"),
+  });
 
-  const email = formData.get("email");
-  const password = formData.get("password");
-  const confirmPassword = formData.get("confirmPassword");
-
-  console.log(email, password, confirmPassword);
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      email: formData.get("email"),
+    };
+  }
 }
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,11 +1,11 @@
 "use client";
+
+import { useActionState } from "react";
 import { register } from "@/actions/auth";
 import Link from "next/link";
-import { useActionState } from "react";
 
 export default function Register() {
   const [state, action, isPending] = useActionState(register, undefined);
-  console.log(isPending);
 
   return (
     <div className="container w-1/2">
@@ -14,17 +14,33 @@ export default function Register() {
       <form action={action} className="space-y-4">
         <div>
           <label htmlFor="email">Email</label>
-          <input type="text" name="email" />
+          <input type="text" name="email" defaultValue={state?.email} />
+          {state?.errors?.email && (
+            <p className="error">{state.errors.email}</p>
+          )}
         </div>
 
         <div>
           <label htmlFor="password">Password</label>
           <input type="password" name="password" />
+          {state?.errors?.password && (
+            <div className="error">
+              <p>Password must:</p>
+              <ul className="list-disc list-inside ml-4">
+                {state.errors.password.map((err) => (
+                  <li key={err}>{err}</li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
 
         <div>
           <label htmlFor="confirmPassword">Confirm Password</label>
           <input type="password" name="confirmPassword" />
+          {state?.errors?.confirmPassword && (
+            <p className="error">{state.errors.confirmPassword}</p>
+          )}
         </div>
 
         <div className="flex items-end gap-4">
