4f9533736c044b85a107d878a866adffa93b82c7
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2024-12-19 17:05:34 -0800

    add mongodb & register user

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,6 +1,9 @@
 "use server";
 
+import { getCollection } from "@/lib/db";
 import { RegisterFormSchema } from "@/lib/rules";
+import bcrypt from "bcrypt";
+import { redirect } from "next/navigation";
 
 export async function register(state, formData) {
   const validatedFields = RegisterFormSchema.safeParse({
@@ -15,4 +18,34 @@ export async function register(state, formData) {
       email: formData.get("email"),
     };
   }
+
+  const { email, password } = validatedFields.data;
+  const userCollection = await getCollection("users");
+  if (!userCollection) {
+    return {
+      errors: {
+        email: "Database error, please try again later",
+      },
+    };
+  }
+
+  const existingUser = await userCollection.findOne({ email });
+  if (existingUser) {
+    return {
+      errors: {
+        email: "Email already exists in the database",
+      },
+    };
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const result = await userCollection.insertOne({
+    email,
+    password: hashedPassword,
+  });
+
+  //TODO: create session
+
+  redirect("/dashboard");
 }
---
+++ src/lib/db.js
@@ -0,0 +1,31 @@
+const { MongoClient } = require("mongodb");
+import { ServerApiVersion } from "mongodb";
+
+if (!process.env.MONGODB_URI) {
+  throw new Error("MONGODB_URI is not set in env");
+}
+
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function getDb(dbName) {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    return client.db(dbName);
+  } catch (error) {
+    console.error("Error connecting to MongoDB", error);
+    return null;
+  }
+}
+
+export async function getCollection(collectionName) {
+  const db = await getDb("next_blog_db");
+  if (db) return db.collection(collectionName);
+  return null;
+}
