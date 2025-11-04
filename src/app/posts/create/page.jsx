7354bd9d4f6db8ae8625e3662fbb54a1d596072d
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2025-11-05 00:56:33 +0100

    create_post3

--- src/app/posts/create/page.jsx
+++ src/app/posts/create/page.jsx
@@ -1,7 +1,12 @@
-export default async function () {
+import { createPost } from "@/actions/posts";
+import BlogForm from "@/components/BlogForm";
+
+export default async function create() {
   return (
-    <div>
-      <h1>Create a Post Page</h1>
+    <div className="container w-1/2">
+      <h1 className="title">Create a new post</h1>
+
+      <BlogForm handler={createPost} />
     </div>
   );
 }
